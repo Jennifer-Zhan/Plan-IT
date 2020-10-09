814fe17b5c5c57029aa2fb9c030a50b15f08a9cc
Zhan, Jennifer <zhanr@rpi.edu>
2020-10-09 20:31:50 +0800

    Added final version

---
+++ Website/home.js
@@ -0,0 +1,20 @@
+function signOut(){
+
+	sessionStorage.clear();
+
+	var urlParts = window.location.href.split('/');
+	var url = "";
+	for(var i = 0; i < urlParts.length -1; i++){
+		url += urlParts[i] + "/";
+	}
+	url += "index.html";
+	window.location = url;
+
+}
+
+/*function loadProfile(){
+	var profile = JSON.parse(sessionStorage.getItem("profile"));
+
+	$(".profileData").css("display","block");
+	$("#email").text(profile.yu);
+}*/
